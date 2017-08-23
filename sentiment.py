62399200de694e61c311c75d1a8e79dec05f6245
victor-iyiola <javafolabi@gmail.com>
2017-08-23 17:41:43 +0100

    loading the corpus from nltk

--- sentiment.py
+++ sentiment.py
@@ -2,4 +2,8 @@ import nltk
 import random
 from nltk.corpus import movie_reviews
 
-documents = []
+documents = [(list(movie_reviews.words(fileid)), cat) 
+            for cat in movie_reviews.categories()
+            for fileid in movie_reviews.fileids(cat)]
+
+print(documents[0])
