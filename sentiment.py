65b4ae04ffb1b704e3f94e7698eb486a5d4063f5
victor-iyiola <javafolabi@gmail.com>
2017-08-23 17:51:43 +0100

    loaded all movie_review words into memory

--- sentiment.py
+++ sentiment.py
@@ -1,9 +1,11 @@
 import nltk
-import random
 from nltk.corpus import movie_reviews
+import random
 
-documents = [(list(movie_reviews.words(fileid)), cat) 
-            for cat in movie_reviews.categories()
-            for fileid in movie_reviews.fileids(cat)]
+documents = [(list(movie_reviews.words(fileid)), cat)
+              for cat in movie_reviews.categories()
+              for fileid in movie_reviews.fileids(cat)]
 
-print(documents[0])
+all_words = [w for w in movie_reviews.words()]
+all_words = nltk.FreqDist(all_words)
+print(all_words.most_common(20))
