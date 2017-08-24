15b0e880202249184d627a96b4b5e5d2e076a944
victor-iyiola <javafolabi@gmail.com>
2017-08-24 23:08:25 +0100

    Shuffling data documents

--- sentiment.py
+++ sentiment.py
@@ -3,8 +3,9 @@ from nltk.corpus import movie_reviews
 import random
 
 documents = [(list(movie_reviews.words(fileid)), cat)
-              for cat in movie_reviews.categories()
-              for fileid in movie_reviews.fileids(cat)]
+             for cat in movie_reviews.categories()
+             for fileid in movie_reviews.fileids(cat)]
+random.shuffle(documents)
 
 all_words = [w for w in movie_reviews.words()]
 all_words = nltk.FreqDist(all_words)
